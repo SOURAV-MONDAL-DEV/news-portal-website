fbc64e8006cd3368b6b52a315b0994eed7adedf1
SOURAVMONDAL-BD <souravmondalcode@gmail.com>
2022-09-03 23:22:23 +0600

    FAQ Finale

--- catagori.js
+++ catagori.js
@@ -34,6 +34,8 @@ function showNews(newses){
     let newsShort = newses.sort((a, b) => parseFloat(b.total_view) - parseFloat(a.total_view));
     const mainNewsSection = document.getElementById('mainNewsSection')
     mainNewsSection.classList.remove('d-none')
+    const faqSection = document.getElementById('faqSection')
+    faqSection.textContent = '';
     const newsNumberField = document.getElementById('newsNumberField')
     newsNumberField.innerText = `${newses.length} News found for this category`
     const newsBody = document.getElementById('newsBody')
@@ -78,6 +80,17 @@ function showNews(newses){
 }
 
 
+const newsBtn = document.getElementById('newsBtn')
+newsBtn.addEventListener('click', function(){
+  const mainNewsSection = document.getElementById('mainNewsSection')
+    mainNewsSection.classList.remove('d-none')
+    const faqSection = document.getElementById('faqSection')
+    faqSection.textContent = '';
+  console.log('newsbtn clik')
+})
+
+
+
 
 
 
@@ -85,6 +98,52 @@ const faqBtn = document.getElementById('faqBtn')
 faqBtn.addEventListener('click', function(){
   const mainNewsSection = document.getElementById('mainNewsSection')
   mainNewsSection.classList.add('d-none')
-  
-console.log(mainNewsSection)
+  const faqSection = document.getElementById('faqSection')
+  faqSection.textContent = '';
+  const faqDiv = document.createElement('div')
+  faqDiv.innerHTML = `
+  <h1 class="text-center fw-bold " >FAQ</h1>
+  <p class="text-center fw-bold " >Frequiently Asked Question</p>
+  <div class="accordion" id="accordionPanelsStayOpenExample">
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+          Var, Let, and Const – What's the Difference?
+        </button>
+      </h2>
+      <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+        <div class="accordion-body">
+          <strong>Var.</strong>
+          Before the advent of ES6, var declarations ruled. There are issues associated with variables declared with var, though. That is why it was necessary for new ways to declare variables to emerge. <br> <strong>Let.</strong>  is now preferred for variable declaration. It's no surprise as it comes as an improvement to var declarations. It also solves the problem with var that we just covered. Let's consider why this is so. <br><strong>Const.</strong>Variables declared with the const maintain constant values. const declarations share some similarities with let declarations.
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+          What is Difference Between Regular Functions and Arrow Functions?
+        </button>
+      </h2>
+      <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+        <div class="accordion-body">
+          <strong>Regular functions</strong> created using function declarations or expressions are constructible and callable. Since regular functions are constructible, they can be called using the new keyword. However, the arrow functions are only callable and not constructible, i.e arrow functions can never be used as constructor functions.
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+         What is the diffeWhat rence between map, for each, filter and find in js?
+        </button>
+      </h2>
+      <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+        <div class="accordion-body">
+          <strong>map</strong> returns an array with the same length, <strong>filter</strong> as the name implies, it returns an array with less items than the original array. reduce returns a single value (or object) <strong>find</strong> returns the first items in an array that satisfies a condition.
+        </div>
+      </div>
+    </div>
+  </div>
+  `
+  faqSection.appendChild(faqDiv)
+console.log(faqSection)
 })
