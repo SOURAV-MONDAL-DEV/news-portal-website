82507e89acc3f9371e2fb26a40cdc1c30aab7468
SOURAVMONDAL-BD <souravmondalcode@gmail.com>
2022-09-03 18:17:20 +0600

    catagory Loded

--- catagori.js
+++ catagori.js
@@ -2,7 +2,6 @@ const loadCatagories = async() => {
     const url = `https://openapi.programming-hero.com/api/news/categories#`
     const res = await fetch(url);
     const data = await res.json();
-
     showcat(data.data.news_category);
 
 }
@@ -16,14 +15,34 @@ const showcat = catagories =>{
     for(const catagori of catagories){
     let catName = catagori.category_name
     let catId = catagori.category_id
-    const catLi = document.createElement('li')
-    catLi.innerHTML = `<a  onclick="clicCatagori(${catId})" id="" class=" btn btn-light btn-sm text-decoration-none text-secondary fs-6" href="">${catName}</a>`
+    const catLi = document.createElement('div')
+    catLi.innerHTML = `<button  onclick="loadNews('${catagori.category_id}')" id="" class=" btn btn-light btn-sm text-decoration-none text-secondary fs-6" href="">${catName}</button>`
     catContainer.appendChild(catLi)
     }
 }
 
-function clicCatagori(catagoriId){
+// function loadNews(catagoriId){
+//     const url = `https://openapi.programming-hero.com/api/news/category/0${catagoriId}`
+//     console.log(url)
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(data => console.log(data.data))
+// }
 
-    console.log(catagoriId)
+
+
+function loadNews(catagoriId){
+    const url = `https://openapi.programming-hero.com/api/news/category/${catagoriId}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showNews(data.data))
 }
-    
+
+function showNews(newses){
+    const newsBody = document.getElementById('newsBody')
+    newses.forEach(news => {
+        
+        console.log(news)
+    });
+}
+
